4d7401df5355ca3d41e8c35409846430e7607454
Julia Finocchiaro <jgfino.dev@gmail.com>
2025-03-18 23:40:51 -0400

    support completing multiple identifiers

--- components/web/autocomplete.tsx
+++ components/web/autocomplete.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import { EquationEnvironment } from "@/lib/types/identifiers";
 
 interface AutocompleteProps {
   x: number;
   y: number;
+  equationId?: string;
   environment: EquationEnvironment;
   input: string;
   numSuggestions?: number;
@@ -11,21 +12,44 @@ interface AutocompleteProps {
 }
 
 const Autocomplete = (props: AutocompleteProps) => {
-  const { x, y, environment, input, onOptionSelected, numSuggestions } = props;
+  const { x, y, environment, input, equationId, onOptionSelected, numSuggestions } = props;
 
-  // Filter identifiers based on the input
-  const filteredIdentifiers = [
-    ...environment.variables,
-    ...environment.functions,
-    ...environment.constants
-  ].filter(identifier => identifier.code.toLowerCase().startsWith(input.toLowerCase())).slice(0, numSuggestions || 10); // Limit to 10 suggestions by default
+  const filteredIdentifiers = useMemo(() => {
+    const startsWith = input.split(' ').at(-1)?.trim()
+
+    if (!startsWith) return []
+
+    return Array.from(new Set([
+      ...environment.variables,
+      ...environment.functions,
+      ...environment.constants
+    ].filter(identifier => identifier.code
+      .toLowerCase()
+      .startsWith(startsWith.toLowerCase()) && (equationId ? identifier.equationId !== equationId : true))
+      .map(identifier => identifier.code)
+      .slice(0, numSuggestions || 10))); // Limit to 10 suggestions by default
+  }, [environment.constants, environment.functions, environment.variables, equationId, input, numSuggestions])
+
+  const handleOptionSelected = useCallback((option: string) => {
+    const parts = input.split(' ');
+
+    // This is what we're replacing
+    parts.pop();
+
+    let start = parts.join(' ');
+    if (start.length > 0) {
+      start += ' ';
+    }
+
+    onOptionSelected(`${start}${option}`);
+  }, [input, onOptionSelected]);
 
   // Listen for tab key to select the first suggestion
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Tab' && filteredIdentifiers.length > 0) {
         event.preventDefault();
-        onOptionSelected(filteredIdentifiers[0].code);
+        handleOptionSelected(filteredIdentifiers[0]);
       }
     };
 
@@ -33,20 +57,20 @@ const Autocomplete = (props: AutocompleteProps) => {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [filteredIdentifiers, onOptionSelected]);
+  }, [filteredIdentifiers, handleOptionSelected]);
 
   return (
     <div style={{ left: x, top: y }} className="fixed z-100 bg-white border border-gray-300 rounded shadow-lg mt-2">
       {filteredIdentifiers.map((identifier) => (
         <div
-          key={identifier.code}
+          key={identifier}
           className="p-2 hover:bg-gray-100 cursor-pointer"
           onClick={() => {
-            onOptionSelected(identifier.code)
+            handleOptionSelected(identifier)
           }}
-          onMouseDown={e => e.preventDefault()} // Prevent input blur on click
+          onMouseDown={e => e.preventDefault()} // Prevent dropdown hiding on click
         >
-          {identifier.code}
+          {identifier}
         </div>
       ))}
     </div>
